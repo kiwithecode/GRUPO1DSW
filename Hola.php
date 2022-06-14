127149ad66ef315a3dc7ed83040c62f920d6f1fe
kiwithecode <wowcelodion@gmail.com>
2022-06-14 10:49:55 -0500

    como subir al git

--- Hola.php
+++ Hola.php
@@ -1,1 +1,12 @@
-Hola
+Pasos para subir en sus ramas cambios 
+paso 1 git checkout su_Rama
+paso 2 git add .
+paso 3 git commit -m "cualquier escritura"
+paso 4 git push 
+
+pasos para copiar cambios 
+paso 1 git add .
+paso 2 git commit -m "cualquier escritura"
+paso 3 git git pull origin su_Rama
+paso 4 git add .
+paso 5 git commit -m "cualquier escritura"
