6cc6e58bbe73899a7c6ef588a2781b64f7a096b4
kiwithecode <wowcelodion@gmail.com>
2022-06-16 09:50:39 -0500

    login

--- Hola.php
+++ Hola.php
@@ -7,6 +7,6 @@ paso 4 git push
 pasos para copiar cambios 
 paso 1 git add .
 paso 2 git commit -m "cualquier escritura"
-paso 3 git git pull origin su_Rama
+paso 3 git git pull origin main
 paso 4 git add .
 paso 5 git commit -m "cualquier escritura"
