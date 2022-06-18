e4a579ce3997720979e81fe2baeab11411f74aea
JossG44 <joselyn4garcia@gmail.com>
2022-06-17 22:00:40 -0500

    Resto de vistas añadidas, diseño de index (página principal)

--- index.php
+++ index.php
@@ -26,11 +26,28 @@ if (isset($_SESSION['user_id'])) {
   <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css">
   <link rel="stylesheet" href="assets/css/style.css">
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Courgette&display=swap" rel="stylesheet">
 </head>
 
+<style>
+  body{
+    background-image: url("https://images.alphacoders.com/519/thumb-1920-519509.jpg");
+  }
+  h1{
+    font-family: 'Courgette', cursive;
+    text-shadow: 4px 4px white;
+    font-size: 60px;
+  }
+
+</style>
+  
 <body>
   <?php require 'partials/header.php' ?>
   
-  
-
+  <h1>
+    Bienvenido al sistema de Bar - ESPEL
+  </h1>
+</body>
 </html>
