1709ea4202e2c67ab2a1bd914a584735da91f177
kiwithecode <wowcelodion@gmail.com>
2022-06-20 20:23:51 -0500

    CAMBIOS base de dato 3

--- database.php
+++ database.php
@@ -1,14 +1,14 @@
 <?php
 
-/*$server = 'localhost:3307';
-$username = 'root';
-$password = '';
-$database = 'php_login_database';
+$server = 'localhost:5432';
+$username = 'wwjjsijrrsycym';
+$password = '17c15c4f3af1e0a8f3f9ea043c0110e32ebb5ee82480a00f1fa2514f78dfa0bf';
+$database = 'dccd8krjqds0s1';
 
 try {
   $conn = new PDO("mysql:host=$server;dbname=$database;", $username, $password);
 } catch (PDOException $e) {
   die('Connection Failed: ' . $e->getMessage());
-}*/
+}
 
 ?>
