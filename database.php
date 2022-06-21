0b7d523ba34a127ef246fffcade36264f485751d
kiwithecode <wowcelodion@gmail.com>
2022-06-20 20:30:59 -0500

    CAMBIOS base de dato 4

--- database.php
+++ database.php
@@ -1,6 +1,6 @@
 <?php
 
-$server = 'localhost:5432';
+$server = 'ec2-54-157-16-196.compute-1.amazonaws.com';
 $username = 'wwjjsijrrsycym';
 $password = '17c15c4f3af1e0a8f3f9ea043c0110e32ebb5ee82480a00f1fa2514f78dfa0bf';
 $database = 'dccd8krjqds0s1';
