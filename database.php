12e17ab9edc49748491877677d1501af5e5044c1
kiwithecode <wowcelodion@gmail.com>
2022-06-20 21:00:54 -0500

    conexion base de datos

--- database.php
+++ database.php
@@ -1,14 +1,18 @@
 <?php
+$B_CONNECTION = 'pgsql';
+$DB_HOST = 'ec2-54-157-16-196.compute-1.amazonaws.com';
+$DB_PORT = '5432';
+$DB_DATABASE = 'dccd8krjqds0s1';
+$DB_USERNAME = 'wwjjsijrrsycym';
 
-$host = 'ec2-54-157-16-196.compute-1.amazonaws.com';
-$database = 'dccd8krjqds0s1';
-$username = 'wwjjsijrrsycym';
-$port = '5432'; 
-$password = '17c15c4f3af1e0a8f3f9ea043c0110e32ebb5ee82480a00f1fa2514f78dfa0bf';
-;
+$DB_PASSWORD = '17c15c4f3af1e0a8f3f9ea043c0110e32ebb5ee82480a00f1fa2514f78dfa0bf';
+
+
+
 
 try {
-  $conn = new PDO("pgsql :host=$$host; dbname=$database; port=$port; $username,$password,");
+  /* $conn = new PDO("pgsql :host=$$host; dbname=$database; port=$port; $username,$password,");*/
+  $conn = new PDO("$B_CONNECTION:host=$DB_HOST;port=$DB_PORT;dbname=$DB_DATABASE", $DB_USERNAME, $DB_PASSWORD);
 } catch (PDOException $e) {
   die('Connection Failed: ' . $e->getMessage());
 }
