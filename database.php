10389b9fc87cba559ba20518a40ae391c1c69208
kiwithecode <wowcelodion@gmail.com>
2022-06-20 20:50:13 -0500

    CAMBIOS base de dato 5

--- database.php
+++ database.php
@@ -1,14 +1,14 @@
 <?php
 
-$server = 'ec2-54-157-16-196.compute-1.amazonaws.com';
-$username = 'wwjjsijrrsycym';
-$password = '17c15c4f3af1e0a8f3f9ea043c0110e32ebb5ee82480a00f1fa2514f78dfa0bf';
+$host = 'ec2-54-157-16-196.compute-1.amazonaws.com';
 $database = 'dccd8krjqds0s1';
+$username = 'wwjjsijrrsycym';
+$port = '5432'; 
+$password = '17c15c4f3af1e0a8f3f9ea043c0110e32ebb5ee82480a00f1fa2514f78dfa0bf';
+;
 
 try {
-  $conn = new PDO("mysql:host=$server;dbname=$database;", $username, $password);
+  $conn = new PDO("psql :host=$$host; dbname=$database; user=$username; pass=$password port=$port");
 } catch (PDOException $e) {
   die('Connection Failed: ' . $e->getMessage());
 }
-
-?>
